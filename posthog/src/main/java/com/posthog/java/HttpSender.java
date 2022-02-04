c1e6fd3249601983973536dc1cbd93d1450ae558
Paul D'Ambra <paul.dambra@gmail.com>
2022-02-04 12:09:00 +0000

    import Response

--- posthog/src/main/java/com/posthog/java/HttpSender.java
+++ posthog/src/main/java/com/posthog/java/HttpSender.java
@@ -10,6 +10,7 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class HttpSender implements Sender {
     private String apiKey;
